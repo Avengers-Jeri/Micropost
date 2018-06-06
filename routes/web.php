f3d9aad366aa3e231cdc59f2aa978d85ef55891d
Avengers-Jeri <kt274.ku58.st@gmail.com>
2018-06-06 07:17:24 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,4 +24,7 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+
+    Route::get('/', 'MicropostsController@index');
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
