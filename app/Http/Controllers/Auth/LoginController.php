2fe77fe38fa16d95fc5edecb0754ce83cb697940
Avengers-Jeri <kt274.ku58.st@gmail.com>
2018-06-14 02:03:10 +0000

    follow, unfollow

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,12 +26,12 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/';
-
+    
     /**
      * Create a new controller instance.
-     *
      * @return void
      */
+     
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -10,7 +10,6 @@ class MicropostsController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
      * @return \Illuminate\Http\Response
      */
     public function index()
